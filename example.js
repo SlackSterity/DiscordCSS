7f888cf1c7fceef4bb37d952061ffc3b02481b21
SlackSterity <38511939+SlackSterity@users.noreply.github.com>
2018-05-07 21:46:17 +0200

    CSS: removed

--- example.js
+++ example.js
@@ -11,15 +11,3 @@
 // ==/UserScript==
 
 GM_addStyle (GM_getResourceText ("myCustomCss") );
-
-/*GM_addStyle ( `
-    * {
-        background-color: blue!important;
-        background: url('http://www.pxleyes.com/images/contests/kiwis/fullsize/sourceimage.jpg') no-repeat center center fixed !important;
-        -webkit-background-size: cover !important;
-        -moz-background-size: cover !important;
-        -o-background-size: cover !important;
-        background-size: cover !important;
-    }
-` );
-*/
